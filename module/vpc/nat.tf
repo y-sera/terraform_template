cf55a17f6da87d2bb2c62467bfbc6c1905ff1a6f
yasuaki <yasuaki.sera15@gmail.com>
2023-07-27 21:13:55 +0900

    fix from count to for_each

--- module/vpc/nat.tf
+++ module/vpc/nat.tf
@@ -1,10 +1,10 @@
 resource "aws_nat_gateway" "nat" {
-  count = length(var.availability_zones)
+  for_each = toset(var.availability_zones)
 
-  allocation_id = aws_eip.eip[count.index].id
-  subnet_id = aws_subnet.pub_subnet[count.index].id
+  allocation_id = aws_eip.eip[each.key].id
+  subnet_id = aws_subnet.pub_subnet[each.key].id
 }
 
 resource "aws_eip" "eip" {
-  count = length(var.availability_zones)
+  for_each = toset(var.availability_zones)
 }
--- module/vpc/subnet.tf
+++ module/vpc/subnet.tf
@@ -1,13 +1,13 @@
 resource "aws_subnet" "pub_subnet" {
-    count = length(var.availability_zones)
+    for_each = toset(var.availability_zones)
     vpc_id = aws_vpc.vpc.id
-    cidr_block = cidrsubnet(var.vpc_cidr_block,8,count.index)
-    availability_zone = var.availability_zones[count.index]
+    cidr_block = cidrsubnet(var.vpc_cidr_block,8,index(var.availability_zones,each.key))
+    availability_zone = each.key
 }
 
 resource "aws_subnet" "pri_subnet" {
-    count = length(var.availability_zones)
+    for_each = toset(var.availability_zones)
     vpc_id = aws_vpc.vpc.id
-    cidr_block = cidrsubnet(var.vpc_cidr_block,8,4 + count.index)
-    availability_zone = var.availability_zones[count.index]
+    cidr_block = cidrsubnet(var.vpc_cidr_block,8,4 + index(var.availability_zones,each.key))
+    availability_zone = each.key
 }
