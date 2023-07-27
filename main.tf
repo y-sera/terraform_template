c7b1949b692bf5b39d1fb09f69b717e2cea7c737
yasuaki <yasuaki.sera15@gmail.com>
2023-07-28 00:14:46 +0900

    fix: terraform fmt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "./module/vpc"
-    vpc_cidr_block = "10.0.0.0/16"
-    prefix = "test"
+  source         = "./module/vpc"
+  vpc_cidr_block = "10.0.0.0/16"
+  prefix         = "test"
 }
