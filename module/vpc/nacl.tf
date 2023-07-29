041dcf7e0b51f403574e7b9e5be32f980aff5420
yasuaki <yasuaki.sera15@gmail.com>
2023-07-30 01:31:02 +0900

    fix: default nacl lifecycle

--- module/vpc/nacl.tf
+++ module/vpc/nacl.tf
@@ -1,6 +1,9 @@
 resource "aws_default_network_acl" "vpc_default_nacl" {
   default_network_acl_id = aws_vpc.vpc.default_network_acl_id
 
+  lifecycle {
+    ignore_changes = [subnet_ids]
+  }
   tags = {
     Name = "${var.prefix}-vpc-default-nacl"
   }
